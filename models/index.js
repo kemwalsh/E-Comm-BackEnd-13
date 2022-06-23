a285373ca203bca4520ad09f8268954042d7eec3
Kelly Walsh <kellywalsh827@gmail.com>
2022-06-23 10:28:44 -0600

    complete index.js

--- models/index.js
+++ models/index.js
@@ -5,12 +5,24 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'product_id',
+});
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+});
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongToMany(Tag, {
+  foreignKey: 'ProductTag',
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongToMany(Product, {
+  foreignKey: 'ProductTag',
+});
 
 module.exports = {
   Product,
