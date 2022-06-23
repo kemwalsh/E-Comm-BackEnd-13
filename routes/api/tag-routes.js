2cf5ec3e6fa91df6dae1f794da84b2a621a14685
Kelly Walsh <kellywalsh827@gmail.com>
2022-06-23 12:09:26 -0600

    add tag routes

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -6,23 +6,50 @@ const { Tag, Product, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all tags
   // be sure to include its associated Product data
+  Tag.findAll({
+    include: [Product],
+  }).then((allCategories) => {
+    res.json(allCategories)
+  })
 });
 
 router.get('/:id', (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  Tag.findByPk(req.params.id, {
+    include: [Product],
+  }).then((allCategories) => {
+    res.json(allCategories)
+  })
 });
 
 router.post('/', (req, res) => {
   // create a new tag
+  Tag.create(req.body).then((allCategories) => {
+    res.json(allCategories)
+  })
 });
 
 router.put('/:id', (req, res) => {
   // update a tag's name by its `id` value
+  Tag.update(req.body, {
+    where: {
+      id: req.params.id,
+    }
+  }).then((allCategories) => {
+    res.json(allCategories)
+  })
 });
 
 router.delete('/:id', (req, res) => {
   // delete on tag by its `id` value
+  Tag.destroy( {
+    where: {
+      id: req.params.id,
+    }
+  }).then((allCategories) => {
+    res.json(allCategories)
+  })
 });
 
 module.exports = router;
