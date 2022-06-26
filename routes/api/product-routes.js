417f408c78db300edfe7bac95ac59f6bc4150606
Kelly Walsh <kellywalsh827@gmail.com>
2022-06-25 23:50:07 -0600

    add delete product route

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -7,12 +7,22 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  Product.findAll({
+    include: [Category, Tag],
+  }).then((allCategories) => {
+    res.json(allCategories)
+  })
 });
 
 // get one product
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  Product.findByPk(req.params.id, {
+    include: [Category, Tag],
+  }).then((allCategories) => {
+    res.json(allCategories)
+  })
 });
 
 // create new product
@@ -91,6 +101,13 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete one product by its `id` value
+  Product.destroy( {
+    where: {
+      id: req.params.id,
+    }
+  }).then((allCategories) => {
+    res.json(allCategories)
+  })
 });
 
 module.exports = router;
